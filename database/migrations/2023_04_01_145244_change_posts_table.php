2847143e176e743efa6ec8184dc11c87d88a2ab5
yuriikudaltsev <kudaltsev@gmail.com>
2023-04-01 19:18:37 +0300

    learn migration

--- database/migrations/2023_04_01_145244_change_posts_table.php
+++ database/migrations/2023_04_01_145244_change_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class ChangePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title');
-            $table->text('content');
-            $table->timestamps();
+         Schema::table('posts', function (Blueprint $table) {
+            $table->string('title', 100)->change();
+            $table->text('content')->nullable()->change();
         });
     }
 
@@ -28,6 +26,7 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        //
     }
+    
 }
